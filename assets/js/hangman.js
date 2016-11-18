5852d4c747c5bb5471ee3af1e2d9650116cdac24
Alex Rosenkranz <alexrosenkranz@Alexs-iMac.local>
2016-11-18 13:20:35 -0500

    More styling of page and added some more words for game

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,17 +1,23 @@
+ // DECLARE VARIABLES NEEDED FOR GAME
+
     var artists = [];
       artists[0] = {name: "kaytranada", youtube: "https://www.youtube.com/embed/rKlA5tRu6f0?rel=0"};
-      artists[1] = {name: "hotelier", youtube: "https://www.youtube.com/embed/AuEMMawhMZU?rel=0"};
+      artists[1] = {name: "the hotelier", youtube: "https://www.youtube.com/embed/AuEMMawhMZU?rel=0"};
       artists[2] = {name: "american football", youtube: "https://www.youtube.com/embed/-9f4dP6jw1Q?rel=0"};
       artists[3] = {name: "a tribe called quest", youtube: "https://www.youtube.com/embed/BDxKVYUHBdA?rel=0"};
       artists[4] = {name: "rufus du sol", youtube: "https://www.youtube.com/embed/Tx9zMFodNtA?rel=0"};
+      artists[4] = {name: "st lucia", youtube: "https://www.youtube.com/embed/Hcm7VFDSU_0?rel=0"};
+      artists[5] = {name: "badbadnotgood", youtube: "https://www.youtube.com/embed/UWIIPX_5rbM?rel=0"};
+      artists[6] = {name: "pinegrove", youtube: "https://www.youtube.com/embed/w9mlfpTxwQA?rel=0"};
 
- // DECLARE VARIABLES NEEDED FOR GAME
-    // var wordsArray = ['kaytranada','hotelier','american football','restorations', 'a tribe called quest', 'rufus du sol'];
     var word;
+    var youtube;
     var wins = 0;
     var losses = 0;
     var lettersGuessed;
     var goesLeft;
+    var randomArtist;
+    var pickedArtist;
     var placeholder;
     var input;
     var wordLength;
@@ -24,16 +30,17 @@
       placeholder = "";
       goesLeft = 10;
       lettersGuessed = [];
-      var randomArtist = Math.floor(Math.random() * artists.length); 
-      var pickedArtist = artists.splice(randomArtist, 1);
+      randomArtist = Math.floor(Math.random() * artists.length); 
+      pickedArtist = artists.splice(randomArtist, 1);
       word =  pickedArtist[0].name;
+      youtube = pickedArtist[0].youtube;
       console.log(word); 
       // word = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 
       wordLength = word.length;
       wordSubstring = word.substring;
     
-    // ENTERS UNDERSCORES FOR LETTERS
+    // ENTERS UNDERSCORES FOR LETTERS AND PREPOPULATES BLANK SPACES
     for (var i = 0; i < word.length; i++) {
       if (word.substring(i, i + 1) === " "){
         placeholder =  placeholder + " ";
@@ -59,9 +66,14 @@
   // LOG KEYSTROKE TO BEGIN LOGIC CHECK
   document.onkeyup = function(event){
     key = event.key;
+
+    
     document.querySelector("#message").innerHTML = "Try and guess who my favorite artists were this year.<br/> Good luck!";
+
+    // SET VARIABLE TO CHECK IF YOU LOSE A TURN UPON INCORRECT GUESS
     var correct = 0;
 
+    // LOOP THROUGH WORD TO CHECK IF LETTER IS CORRECT
     for (var i = 0; i < wordLength; i++) {
       if (key == word.substring(i, i + 1)) {
         correct++;
@@ -70,6 +82,7 @@
       }
     }
 
+    // IF LETTER SELECTED WASN'T CORRECT
     if (correct == 0) {
       goesLeft--;
       document.querySelector("#guessesLeft").innerHTML = goesLeft;
@@ -77,16 +90,24 @@
       document.querySelector("#lettersGuessedSpace").innerHTML = lettersGuessed;
     }
 
+    // WHEN YOU WIN
     if (placeholder == word) {
       wins++;
       document.querySelector("#message").innerHTML = "You win! You must have good music taste.<br/>Click the Restart Game button to play again!";
+
       document.querySelector("#wins").innerHTML = wins;
-
+      
+      document.querySelector("#name").innerHTML = "Artist Name<br/>" + word;
+      
+      document.querySelector("#youtube").innerHTML = '<iframe height="315" src="'+ youtube +'"" frameborder="0" allowfullscreen></iframe>';
+      
     }
-
+    // WHEN YOU LOSE
     if (goesLeft == 0) {
       losses++;
+      
       document.querySelector("#message").innerHTML = "Tough break, but you should check out the music anyway.<br/>Click the Restart Game button to play again!";
+      
       document.querySelector("#losses").innerHTML = losses;
     }
 
